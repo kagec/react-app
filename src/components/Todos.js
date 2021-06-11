e2ecd778432f0d104b3df0e7903f62b1d55ce9e2
kagec <66302618+kagec@users.noreply.github.com>
2021-06-11 16:54:18 +0900

    feat: Todos.js 追加

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import Todo from "./Todo";
+
+const Todos = ({ todos, onDelete }) => {
+  return (
+    <>
+      {todos.map((todo, index) => (
+        <Todo key={index} todo={todo} onDelete={onDelete} />
+      ))}
+    </>
+  );
+};
+
+export default Todos;
