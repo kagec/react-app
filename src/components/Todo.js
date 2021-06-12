9aa9750ab647c2c4d7b8e72b5d30286cbccba852
kagec <66302618+kagec@users.noreply.github.com>
2021-06-12 09:09:53 +0900

    layout: 長文を入力するとはみ出していた問題を修正

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,11 @@ import { FaTimes } from "react-icons/fa";
 const Todo = ({ todo, onDelete }) => {
   return (
     <div className="todo">
-      <h2>
-        {todo.task}
-        <FaTimes
-          style={{ color: "red", cursor: "pointer" }}
-          onClick={() => onDelete(todo.id)}
-        />
-      </h2>
+      <h2>{todo.task}</h2>
+      <FaTimes
+        style={{ color: "red", cursor: "pointer" }}
+        onClick={() => onDelete(todo.id)}
+      />
     </div>
   );
 };
