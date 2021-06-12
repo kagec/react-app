e57a4da4273f9f49234dc95e9fc3a097205be519
kagec <66302618+kagec@users.noreply.github.com>
2021-06-12 11:16:49 +0900

    fix: × が昨日来ていない不具合の修正

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,13 @@ import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 
 const Todo = ({ todo, onDelete, onClick }) => {
   return (
-    <div
-      className={`todo ${todo.completed && "completed"}`}
-      onClick={() => onClick(todo.id)}
-    >
+    <div className={`todo ${todo.completed && "completed"}`}>
       <h2>{todo.task}</h2>
-      {todo.completed ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />}
+      {todo.completed ? (
+        <ImCheckboxChecked onClick={() => onClick(todo.id)} />
+      ) : (
+        <ImCheckboxUnchecked onClick={() => onClick(todo.id)} />
+      )}
       <FaTimes
         style={{ color: "red", cursor: "pointer" }}
         onClick={() => onDelete(todo.id)}
