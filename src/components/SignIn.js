1e495d1491139db5c4020513ce6e02d3ca9fbf20
kagec <66302618+kagec@users.noreply.github.com>
2021-06-21 14:43:00 +0900

    feat: Context を使用するように変更

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { useAuth } from "./ProvideAuth";
 
-const SignIn = ({ setSignIn }) => {
+const SignIn = () => {
+  const { signIn } = useAuth();
+
   return (
     <div>
       <h4>SignIn</h4>
-      <Button color="green" text="Sign In" onClick={setSignIn} />
+      <Button color="green" text="Sign In" onClick={signIn} />
 
       <p>
         未登録の方は<Link to="signup">Sign Up </Link>
