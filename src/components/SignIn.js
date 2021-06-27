8721618dec0de77f2f3c5397ce5ae37097a5be2b
kagec <66302618+kagec@users.noreply.github.com>
2021-06-27 16:38:57 +0900

    feat: サインイン用入力フォーム

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,7 +8,11 @@ const SignIn = () => {
   return (
     <div>
       <h4>SignIn</h4>
-      <Button color="green" text="Sign In" onClick={signIn} />
+      <form>
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="password" />
+        <input className="btn" type="submit" value="Sign In" />
+      </form>
 
       <p>
         未登録の方は<Link to="signup">Sign Up </Link>
