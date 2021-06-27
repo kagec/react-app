8f13ca6e6e352e454dff54044e65f654540aa3a7
kagec <66302618+kagec@users.noreply.github.com>
2021-06-27 18:10:28 +0900

    feat: input にフックを追加

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./ProvideAuth";
 
 const SignIn = () => {
   const { signIn } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div>
       <h4>SignIn</h4>
       <form>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <input className="btn" type="submit" value="Sign In" />
       </form>
 
