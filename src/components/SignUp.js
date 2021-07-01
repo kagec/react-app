00a828a8cd6a7a1ca7a938ebe99e2702d9e525e9
kagec <66302618+kagec@users.noreply.github.com>
2021-07-01 16:43:04 +0900

    feat: 入力フォームの作成 #21

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h4>SignUp</h4>
+      <form>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input className="btn" type="submit" value="Sign Up" />
+      </form>
       <p>
         登録済みの方は<Link to="signin">Sign In </Link>
       </p>
