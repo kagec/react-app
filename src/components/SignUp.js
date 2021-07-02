2fc57b6738605ea2e29c7f2f7d998f33c3f2f7bf
kagec <66302618+kagec@users.noreply.github.com>
2021-07-02 23:26:30 +0900

    不要な関数とその使用箇所を削除

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,30 +6,10 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const authSignUp = async () => {
-    try {
-      const { data } = await axios.post("http://localhost:5000/auth/signup", {
-        email,
-      });
-
-      return data.token;
-    } catch (e) {
-      throw new Error(e.message);
-    }
-  };
-
   const onSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const token = await authSignUp();
-
-      if (token) {
-        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-      } else {
-        axios.defaults.headers.common["Authorization"] = null;
-      }
-
       await axios.post("http://localhost:5000/users", {
         email,
         password,
