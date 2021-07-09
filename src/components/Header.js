f2a6d1d82c015de99f1536c36efcea6da303c6f9
kagec <66302618+kagec@users.noreply.github.com>
2021-07-10 08:24:52 +0900

    サインアウト機能の追加 #28

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
+import { useAuth } from "./ProvideAuth";
 
 const Header = ({ title }) => {
+  const { signOut } = useAuth();
+
+  const onClick = () => {
+    localStorage.clear();
+    signOut();
+  };
+
   return (
     <header>
       <h1>{title}</h1>
+      <Button color={"darkblue"} text={"Sign Out"} onClick={onClick} />
     </header>
   );
 };
